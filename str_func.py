4a697b63af6910cd5bd549afaa7e47a209cfcd70
Alexandra Sazhaeva <alexa.sazhaeva@gmail.com>
2023-06-27 02:23:26 +0200

    Create function up_first_letter

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def string_to_up(string):
+    """Делает все буквы заглавными"""
     return string.upper()
+
+
+def up_first_letter(string):
+    """Делает заглавной первую букву слова"""
+    return string.title()
